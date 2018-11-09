e849478ad9bdfbd2f604dedfab5beea01b226fad
= <rlokesh@aconex.com>
2018-11-09 23:26:06 +0530

    created todoheader component which renders header

---
+++ src/main/resources/static/app.js
@@ -0,0 +1,11 @@
+import {TodoHeader} from "./todoheader";
+
+const React = require('react');
+const ReactDOM = require('react-dom');
+
+class App extends React.Component{
+    render(){
+        <TodoHeader/>,document.getElementById("app")
+    }
+
+}
---
+++ src/main/resources/static/todoheader.js
@@ -0,0 +1,13 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+
+export class TodoHeader extends React.Component{
+    render(){
+        return(
+            <div id="todoHeader" className={"header"}>
+                <h1>todos</h1>
+            </div>
+        )
+    }
+
+}
