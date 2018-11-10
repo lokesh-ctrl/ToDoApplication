d687cdec14df23659c0e7e213042f8bebd667c32
= <rlokesh@aconex.com>
2018-11-10 23:33:53 +0530

    Getting tasks by calling tasks api

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -2,36 +2,47 @@ import {Index} from "./indexer";
 
 const React = require('react');
 const ReactDOM = require('react-dom');
+const axios = require('axios');
 
 export class App extends React.Component {
     constructor(props) {
         super(props)
-        this.state = {tasks: [
+        this.state = {
+            tasks: [
+                //sample data
                 {
-                    id:1,
-                    taskName:"hi5",
-                    updateStatusOfTask:false
+                    id: 1,
+                    taskName: "hi5",
+                    updateStatusOfTask: false
                 },
                 {
-                    id:2,
-                    taskName:"hi4",
-                    updateStatusOfTask:false
+                    id: 2,
+                    taskName: "hi4",
+                    updateStatusOfTask: false
                 },
                 {
-                    id:4,
-                    taskName:"hi3",
-                    updateStatusOfTask:false
+                    id: 4,
+                    taskName: "hi3",
+                    updateStatusOfTask: false
                 },
                 {
-                    id:5,
-                    taskName:"hi2",
-                    updateStatusOfTask:false
+                    id: 5,
+                    taskName: "hi2",
+                    updateStatusOfTask: false
                 }
-            ]}
+            ]
+        }
     }
 
     componentDidMount() {
-
+        axios({
+            method: 'get',
+            url: '/tasks',
+            responseType: 'application/json'
+        }).then(function (response) {
+            console.log(response)
+            this.setState({tasks: response})
+        })
     }
 
     render() {
